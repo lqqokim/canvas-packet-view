f3a8da166bf04f497da2c8ae04da8cbdcf4f3792
kiminsu <kis420404@gmail.com>
2019-03-05 08:06:15 +0900

    add shape

--- canvas.js
+++ canvas.js
@@ -11,43 +11,67 @@ if (canvas.getContext) {
   const ctx = canvas.getContext('2d');
 
   //circle
-  drawCircle(ctx);
-  drawLines(ctx);
-}
+  circle(ctx);
+  lines(ctx);
 
-function drawCircle(ctx) {
-  // ctx.beginPath();
-  ctx.strokeStyle = "#426C9D";
-  ctx.fillStyle = "#426C9D";
-  // ctx.globalAlpha = 0.2;
+  for (let i = 0; i < 1200; i++) {
+    move(i);
+  }
 
-  for (var i = 0; i < 7; i++) {
-    ctx.beginPath();
-    ctx.arc(50, 50, 10 + 0.3 * i, 0, Math.PI * 2, true);
-    ctx.fill();
+  function move(x) {
+    setTimeout(function () {
+      ctx.clearRect(0, 0, 300, 600)
+      ctx.beginPath();
+      x = x % 300;
+      ctx.fillRect(x, 550, 50, 30);
+    }, 3 * x);
   }
 }
 
-function drawLines(ctx) {
-  ctx.beginPath();
-  ctx.moveTo(60, 48);
-  ctx.lineTo(400, 48);
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 1;
-  ctx.stroke();
+function circle(ctx) {
+
+  ctx.strokeStyle = "#426C9D";
+  // ctx.fillStyle = "rgb(66,108,157, 0.9)"
+
+  // ctx.beginPath();
+  // ctx.arc(200, 60, 10, 30 * Math.PI / 180, 120 * Math.PI / 180, true);
+  for (let i = 0; i < 5; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = `rgb(66,108,157,${0.2 * i})`
+    ctx.arc(150, 260, 5 * i, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+  // ctx.arc(150, 60, 20, 0, Math.PI * 2, true);
+  // ctx.fill();
 
   ctx.beginPath();
-  ctx.moveTo(60, 52);
-  ctx.lineTo(400, 52);
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 1;
+  ctx.moveTo(50, 260);
+  ctx.fillStyle = "rgb(66,108,157, 0.3)"
+  ctx.lineTo(150, 240);
+  ctx.lineTo(150, 280);
+  ctx.fill();
+}
+
+function lines(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(50, 260);
+  ctx.lineTo(500, 260);
+  ctx.strokeStyle = "rgb(255, 255, 255, 0.1)";
+  ctx.lineWidth = 2;
   ctx.stroke();
 
-  
-  ctx.beginPath();
-  ctx.moveTo(400, 0);
-  ctx.lineTo(400, 100)
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 3;
-  ctx.stroke();
+  // ctx.beginPath();
+  // ctx.moveTo(60, 52);
+  // ctx.lineTo(400, 52);
+  // ctx.strokeStyle = "white";
+  // ctx.lineWidth = 1;
+  // ctx.stroke();
+
+
+  // ctx.beginPath();
+  // ctx.moveTo(400, 0);
+  // ctx.lineTo(400, 100)
+  // ctx.strokeStyle = "white";
+  // ctx.lineWidth = 3;
+  // ctx.stroke();
 }
